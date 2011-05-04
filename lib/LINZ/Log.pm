289c27a7375d9f1bbfc2c743f1566d8684ed433b
Jeremy Palmer <jpalmer@linz.govt.nz>
2011-05-04 21:52:46 +0000

    Add build system for BDE and Utils perl packages
    git-svn-id: svn://localhost/perl_lib/Utils@1202 40957e5d-2553-1d4f-9b5d-fe299c199263

--- lib/LINZ/Log.pm
+++ lib/LINZ/Log.pm
@@ -1,5 +1,17 @@
+################################################################################
+#
 # $Id$
-
+#
+# LINZ Log Perl package
+#
+# Copyright 2011 Crown copyright (c)
+# Land Information New Zealand and the New Zealand Government.
+# All rights reserved
+#
+# This program is released under the terms of the new BSD license. See the 
+# LICENSE file for more information.
+#
+################################################################################
 package LINZ::Log;
 
 use strict;
