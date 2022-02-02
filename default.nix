63886bd4cc2fd885e79d2881684dffde46dde1d7
Victor Engmark <vengmark@linz.govt.nz>
2022-02-03 11:44:12 +1300

    feat: Add dependencies for initial testing steps
    Enables building with Nix:
    
    ```shell
    nix-build --keep-failed --expr 'with import <nixpkgs> {}; callPackage ./default.nix {}'
    ```
    
    and running tests:
    
    ```shell
    nix-shell --pure --run './configure.bash && make && make check RUNTESTFLAGS=--verbose && make dist && make distcheck && make disttest'
    ```
    
    `make deb` doesn't work, because of the following error:
    
    > dpkg-checkbuilddeps: error: cannot open /var/lib/dpkg/status: No such
    > file or directory
    
    I'm not sure how to avoid that.

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+pkgs.buildPerlPackage {
+  pname = "LINZ-Utils";
+
+  version = "0.0.1";
+
+  src = pkgs.lib.cleanSource ./.;
+
+  propagatedBuildInputs = [
+    pkgs.perlPackages.ModuleBuild
+    pkgs.perlPackages.TestException
+  ];
+
+  preConfigure = ''
+    patchShebangs ./configure.bash
+    ./configure.bash
+  '';
+}
--- shell.nix
+++ shell.nix
@@ -7,14 +7,27 @@
       })
     { }
 }:
+let
+  package = import ./default.nix {
+    inherit pkgs;
+  };
 
+  perlEnv = pkgs.perl.withPackages (
+    ps: [
+      package
+    ]
+  );
+in
 pkgs.mkShell {
   buildInputs = [
     pkgs.bashInteractive
     pkgs.cacert
     pkgs.docker
+    pkgs.dpkg
+    pkgs.fakeroot
     pkgs.gitFull
     pkgs.nodejs
+    perlEnv
     pkgs.pre-commit
     pkgs.which
   ];
